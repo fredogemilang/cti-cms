093d5b6490ccf7c1612ed35e84793447ee26033d
alfredrahardian <fredogemilang@gmail.com>
2026-08-27 13:30:46 +0700

    fix(cdt): add paragraph spacing and margin to client says testimonial content

--- themes/cdt/views/partials/deferred/testimonials.blade.php
+++ themes/cdt/views/partials/deferred/testimonials.blade.php
@@ -82,8 +82,8 @@
                       fill="currentColor" viewBox="0 0 24 24">
                       <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
                     </svg>
-                    <div class="relative z-10">
-                      <div class="text-base md:text-lg text-dark font-light leading-relaxed mb-6">{!! $testimonialContent !!}</div>
+                    <div class="relative z-10 w-full">
+                      <div class="text-base md:text-lg text-dark font-light leading-relaxed mb-6 space-y-4 [&_p]:mb-4 last:[&_p]:mb-0">{!! $testimonialContent !!}</div>
                     </div>
                   </div>
                 </div>
--- themes/cdt/views/single-technology-alliance.blade.php
+++ themes/cdt/views/single-technology-alliance.blade.php
@@ -607,8 +607,8 @@
               </div>
               <div class="lg:w-2/3 p-10 md:p-12 flex items-center relative">
                 <svg class="absolute top-8 left-8 w-24 h-24 text-zinc-100 -z-10 transform -scale-x-100" fill="currentColor" viewBox="0 0 24 24"><path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" /></svg>
-                <div class="relative z-10">
-                  <div class="text-base md:text-lg text-zinc-900 font-light leading-relaxed">{!! $storyContent !!}</div>
+                <div class="relative z-10 w-full">
+                  <div class="text-base md:text-lg text-zinc-900 font-light leading-relaxed space-y-4 [&_p]:mb-4 last:[&_p]:mb-0">{!! $storyContent !!}</div>
                 </div>
               </div>
             </div>
